9784259e3e8af0c8e1d1c7886398b269ae621151
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-03-04 17:07:11 +0500

    webview screen added

---
+++ app/src/main/java/com/example/webview_kotlin_jetpack_compose/ui/WebView/WebViewScreen.kt
@@ -0,0 +1,30 @@
+package com.example.webview_kotlin_jetpack_compose.ui.WebView
+
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WebViewScreen(url: String){
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.loadWithOverviewMode = true
+                settings.useWideViewPort = true
+            }
+
+        },
+        update = {
+            it.loadUrl(url)
+        }
+    )
+}
